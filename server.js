75be53353000e3e17010c1ca08532cadd0d9d705
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-08-06 10:30:11 +0200

    little change in server.js

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ app.use(bodyParser.json());
 
 app.get('/getNote', function (req, res) {
     fs.readFile('./test.json', 'utf8', function(err, data) {
-    if (err) throw err;
-    stringifyFile = data
-    res.send(data);
-});
+	    if (err) throw err;
+	    stringifyFile = data;
+	    res.send(data);
+	});
 });
 
 app.post('/upload/:id', function (req, res) {
     fs.writeFile('./test.json',stringifyFile, function(err) {
-    if (err) throw err;
-    stringifyFile = req.params.id;
-});
+	    if (err) throw err;
+	    stringifyFile = req.params.id;
+	});
 });
 
 app.listen(3000);
